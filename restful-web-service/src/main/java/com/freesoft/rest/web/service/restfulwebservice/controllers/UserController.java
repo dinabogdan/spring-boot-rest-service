bffb0b5fd004aa119c32d2e3cd5a1686856eedc8
dinabogdan <bogdan.dina03@gmail.com>
2018-03-03 22:11:59 +0200

    some refactoring

--- restful-web-service/src/main/java/com/freesoft/rest/web/service/restfulwebservice/controllers/UserController.java
+++ restful-web-service/src/main/java/com/freesoft/rest/web/service/restfulwebservice/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.freesoft.rest.web.service.restfulwebservice.controllers;
 import com.freesoft.rest.web.service.restfulwebservice.beans.User;
 import com.freesoft.rest.web.service.restfulwebservice.dao.UserDAOService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,12 +27,21 @@ public class UserController {
     }
 
     @PutMapping(value = "/users")
-    public void addUser(@RequestBody User user) {
+    public ResponseEntity<Object> addUser(@RequestBody User user) {
         User savedUser = userService.save(user);
+        URI uriLocation = ServletUriComponentsBuilder.
+                fromCurrentRequest().
+                path("/{id}").
+                buildAndExpand(savedUser.getId()).
+                toUri();
+        return ResponseEntity.
+                created(uriLocation).
+                build();
     }
 
+
     @DeleteMapping(value = "/users/{userId}")
-    public void deleteUserById(@PathVariable int userId){
+    public void deleteUserById(@PathVariable int userId) {
         userService.removeUser(userId);
     }
 }
