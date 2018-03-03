2c62118d19fe379180c3c76a54dc7b27640c15fa
dinabogdan <bogdan.dina03@gmail.com>
2018-03-03 21:09:29 +0200

    add the UserDaoRepository

---
+++ restful-web-service/src/main/java/com/freesoft/rest/web/service/restfulwebservice/dao/UserDAOService.java
@@ -0,0 +1,38 @@
+package com.freesoft.rest.web.service.restfulwebservice.dao;
+
+import com.freesoft.rest.web.service.restfulwebservice.beans.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class UserDAOService {
+
+    private static int usersCount = 3;
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Joschua Bloch", new Date()));
+        users.add(new User(2, "Bruce Eckel", new Date()));
+        users.add(new User(3, "Brian Goetz", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id){
+        return users.stream().filter(u -> u.getId() == id).findFirst().get();
+    }
+
+}
